cf60428833775ee59f7587e037f0c43f42c2ae22
98359 <40171217+98359@users.noreply.github.com>
2018-06-11 15:56:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -4,3 +4,5 @@ const client = new Discord.Client();
 client.on('ready', () => {
   client.user.setGame('-help ', 'https://www.twitch.tv/lucasdavid913/')
 })   
+
+client.login("NDU1NDQ3NTk4NDMwNDIxMDEz.DgAC4w.CGcSX7PO9MLwVrI3CB4UuDHZC_0");
