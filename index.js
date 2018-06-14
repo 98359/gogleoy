0c83a6826e10dfe76551771a2e2786dea607249c
98359 <40171217+98359@users.noreply.github.com>
2018-06-14 19:49:27 +0300

    Update index.js

--- index.js
+++ index.js
@@ -54,6 +54,23 @@ if (message.content.startsWith(prefix + 'serverinfo')) {
 
 client.on('message', message =>{
   let args = message.content.split(" ").slice(1);
+  
+if (message.content.startsWith(prefix + 'botinfo')) {
+  const embed = new Discord.RichEmbed()
+  embed.addField('Name', message.user.name , true)
+  .setTitle('Prefix')
+  .setDescription(`-`)
+  embed.addField('Owner', message.user.owner , true)
+  embed.addField('ID', message.user.id, true)
+  embed.setColor(`ff0000`)
+  embed.setThumbnail(message.user.iconURL)
+  message.channel.sendEmbed(embed)
+}
+  
+});
+
+client.on('message', message =>{
+  let args = message.content.split(" ").slice(1);
 
   if ( message.content.startsWith("https://discord.gg/")) {
     message.delete()
